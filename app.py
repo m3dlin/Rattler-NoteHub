b33f1f935bc09fc9258f189f1c74cb2ffd184550
Emily <emedlin08@gmail.com>
2024-01-16 15:58:06 -0600

    init

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/") #empty route, homepage route
+def index():
+    return render_template('index.html'), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
