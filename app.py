f77f70853ff83da3a796be7cfc3343cd8258f8c6
Emily <emedlin08@gmail.com>
2024-01-28 20:53:20 -0600

    course section added

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+courses = {
+  "CS1310": "Programming I",
+  "TH1301": "Introduction to Theology",
+  "CS3300": "Python for Data Analytics",
+  "CS3340": "Computer Networks",
+  "TH3350": "Moral Theology"
+}
+
+
+
 @app.route("/") #empty route, homepage route
 def index():
-    return render_template('index.html'), 200
+    return render_template('index.html',
+                           courses=courses), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
