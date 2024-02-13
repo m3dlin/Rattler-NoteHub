f8e457e385a5c2b6a9ca8959cf2d2dfa6cbacd71
Emily <emedlin08@gmail.com>
2024-02-13 15:04:52 -0600

    added scraper for courses and collected course data for DB

--- utils/stmu_scraper.py
+++ utils/stmu_scraper.py
@@ -1,8 +1,16 @@
+"""
+This module is used to retrieve all courses from StMU's current catalog
+"""
+
 import requests
 from bs4 import BeautifulSoup
 import time  
+import unidecode
+from sqlalchemy import create_engine, MetaData
+import os
+from database import add_courses_to_DB
 
-def get_course_numbers(url):
+def get_course_info(url):
     response = requests.get("https://catalog.stmarytx.edu"+url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -31,7 +39,8 @@ def get_course_numbers(url):
             course_name = course_text[first_period_index + 2:second_period_index]
 
             # .strip() removes leading/trailing whitespace
-            courseId_list.append(course_id.strip().replace(" ",""))  
+            # unidecode convert all non-ASCII characters to their closest ASCII equivalent (in this case \xa0 turns to a space)
+            courseId_list.append(unidecode.unidecode(course_id.strip().replace(" ","")))  
             course_name_list.append(course_name.strip())
         
         return courseId_list, course_name_list
@@ -40,7 +49,7 @@ def get_course_numbers(url):
         print('Failed to fetch the courses for ' + str(url))
     return
     
-    
+
 def main():
     url = 'https://catalog.stmarytx.edu/undergraduate/course-descriptions/'
     response = requests.get(url)
@@ -61,15 +70,15 @@ def main():
             # for each department, go to the dept. page and retrieve all courses
             for department in departments:
                 department_url = department.find('a') # a tags have hyperlinks to dept page
+                course_ids = []
+                course_names = []
 
                 # as long as there is a url for the department, get course numbers
                 if department_url:
                     department_url = department_url['href']
-                    course_ids,course_names = get_course_numbers(department_url)
+                    course_ids,course_names = get_course_info(department_url)
 
-                    print(course_names[4])
-                    
-
+                    """
                     if course_ids:
                         print(f'Courses for {department.text}:')
                         for id,name in zip(course_ids,course_names):
@@ -77,11 +86,18 @@ def main():
                         print('---')
                     else:
                         print(f'No course numbers found for {department.text}')
+                    """
 
                     time.sleep(3) # timer is used so that the code does not overload the website with requests
+
+                add_courses_to_DB(course_ids, course_names)
+                print('added to database')
     else:
         print('Failed to fetch the web page')
+    
 
-
+"""
 if __name__ == "__main__":
     main()
+"""
+
