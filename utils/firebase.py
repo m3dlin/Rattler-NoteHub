38bc83f3181ce464c92d4e229c22fb64ee83565c
RubenB232 <rubenbecerril.rb@gmail.com>
2024-03-26 14:14:17 -0500

    modified:   templates/add-courses-page.html

---
+++ utils/firebase.py
@@ -0,0 +1,49 @@
+import firebase_admin
+from firebase_admin import credentials, storage
+from database import add_note_to_db
+from urllib.parse import quote
+import uuid
+
+
+# setting up connection to firebase DB using credentials
+# gives access to SDK
+cred = credentials.Certificate("rattler-notehub-firebase.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket' : 'rattler-notehub.appspot.com' #reference to the default storage bucket
+})
+bucket = storage.bucket()
+
+def upload_to_firebase(file):
+    
+    #unique id to distinguish multiple files with same name
+    unique_id = str(uuid.uuid4())
+
+    # upload the file to Firebase Storage
+    file_name = f"{file.split('/')[-1]}_{unique_id}"  # add unique ID to the filename
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(file)
+
+    
+    file_path = blob.name # must use unique ID + filename
+    encoded_file_path = quote(file_path, safe='') # adding % symbol into each space
+
+    bucket_name = "rattler-notehub.appspot.com"
+    url = f"https://firebasestorage.googleapis.com/v0/b/{bucket_name}/o/{encoded_file_path}?alt=media" # file path format to publicly access all files
+
+    add_note_to_db(url)
+
+    return url
+
+
+def delete_file_from_firebase(file):
+    # file needs to be the name of the file
+    # Get the Blob object for the file to delete
+    blob = bucket.blob(file)
+
+    # delete file
+    blob.delete()
+
+
+# testing
+#if __name__ == '__main__': 
+#    print("connected")
